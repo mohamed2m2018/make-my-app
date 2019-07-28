ff327d35d24972ad02323c1f5c2b28f842df09b0
unknown <mohamed2m2018@gmail.com>
2019-07-28 21:28:25 +0200

    Adding some fontawesome packages

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <div className="menu-icon">
+          <div className="line line1" />
+          <div className="line line2" />
+          <div className="line line3" />
+        </div>
+
+        <ul className="social-icons-list">
+          <li>
+            <a href="http://google.com" className="social-link">
+              <i>
+                <FontAwesomeIcon icon={faFacebook} />
+              </i>
+            </a>
+          </li>
+          <li>
+            <a href="http://google.com" className="social-link">
+              <i>
+                <FontAwesomeIcon icon={faTwitter} />
+              </i>
+            </a>
+          </li>
+
+          <li>
+            <a href="http://google.com" className="social-link">
+              <i>
+                <FontAwesomeIcon icon={faInstagram} />
+              </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
