af5ecc5b73da5db2f7577af7ec36b926607d1171
unknown <mohamed2m2018@gmail.com>
2019-08-04 12:32:29 +0200

    Add basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
                   <h3 className='Footer-heading'>Make My App</h3>
                   <ul className='footer-list'>
                       <li className='footer-list-item'>
-                          <a href='#' className='footer-list-link'/>
+                          <a href='#' className='footer-list-link'></a>
                       </li>
                   </ul>
               </div>
